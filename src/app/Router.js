1a27951e7a8cb5668a39796cc21342ce9185322c
Wandole <mchirez@gmail.com>
2022-09-29 16:08:37 +0200

    Add Employees Page

--- src/app/Router.js
+++ src/app/Router.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from '../components/Layout/Layout'
 import Home from '../pages/Home'
-
+import Employees from '../pages/Employees'
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="employees" element={<Employees />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Employees.js
@@ -0,0 +1,10 @@
+function Employees() {
+  return (
+    <main>
+      <h2>Current Employees</h2>
+      <table id="employee-table" class="display"></table>
+    </main>
+  )
+}
+
+export default Employees
