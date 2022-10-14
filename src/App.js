fee12f0c162f6b763f7bef1e6477c9660acfa125
Wandole <mchirez@gmail.com>
2022-10-14 14:05:51 +0200

    Sync comments for mentorat

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ function App() {
 
 export default App
 // TODO: mentor: Je dois ajouter des 'options' à mon composant?
+// TODO: mentor: Issue Tableau: il faut faire en sorte de montrer que les nouvelles lignes apparaissent sans recharger l'ensemble du tableau? (voir issue)
+
 // TODO: mentor: Je dois encore changer le système de stockage
 // TODO: mentor: Je vais faire le test lighthouse avec plus de données dans mon tableau (pour les 2 versions)
 // TODO: mentor: Je dois faire un readme pour le package
-// TODO: mentor: Nettoyer le code (découper en fcts + modularité)
+// TODO: mentor: Nettoyer le code (découper en fcts + modularité) là où c'est encore nécessaire
 // TODO: mentor: Vérifier que les problèmes qui venaient des plugin sont réglés avec la nouvelle implémentation
-// TODO: mentor:
+// TODO: mentor: Changer le style de la modale (pour répondre à l'issue d'origine de cette modale)
