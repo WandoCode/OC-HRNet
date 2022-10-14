f0e3c937bb1be0c87f841d8dec90b359083983f5
Wandole <mchirez@gmail.com>
2022-10-14 13:51:18 +0200

    Sync comments for mentorat

--- src/App.js
+++ src/App.js
@@ -6,3 +6,10 @@ function App() {
 }
 
 export default App
+// TODO: mentor: Je dois ajouter des 'options' à mon composant?
+// TODO: mentor: Je dois encore changer le système de stockage
+// TODO: mentor: Je vais faire le test lighthouse avec plus de données dans mon tableau (pour les 2 versions)
+// TODO: mentor: Je dois faire un readme pour le package
+// TODO: mentor: Nettoyer le code (découper en fcts + modularité)
+// TODO: mentor: Vérifier que les problèmes qui venaient des plugin sont réglés avec la nouvelle implémentation
+// TODO: mentor:
