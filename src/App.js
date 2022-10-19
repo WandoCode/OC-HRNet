251f03443bf6613ee77af0e8dfc50aadd6d9c329
Wandole <mchirez@gmail.com>
2022-10-19 19:45:03 +0200

    Remove comments

--- src/App.js
+++ src/App.js
@@ -16,8 +16,3 @@ function App() {
 }
 
 export default App
-// TODO: mentor: Livrables. Quelles sont les 3 liens à envoyer (le 3eme est flou)
-// TODO: mentor: Test pas faits. Grave? Encore le temps même si ma soutenance est vddi 20h?
-
-// TODO: mentor: Okay avec localStorage?
-// TODO: mentor: Refaire Lighthouse
